1ee59be1920f8a1aa16ef1a4822130507708217c
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2024-02-14 06:53:20 +0530

    Created and connected to DB

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,6 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+
+export const register = async(req,res) =>{
+    
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
-import express from 'exoress';
+import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import userRouter from './routes/user.route.js';
+
+dotenv.config();
+
+const app = express();
+
+const DB = process.env.URI;
+const PORT = process.env.PORT;
+
+const connect = mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log("Connection successful");
+  }).catch((err) => console.error("Connection failed", err));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+
+app.use('/api/user', userRouter); 
